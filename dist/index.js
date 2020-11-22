eb06e66112c371efea2d5c492e472b9ac5a8b197
afeiship <1290657123@qq.com>
2020-11-22 17:45:56 +0800

    Release 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  * description: Filter for next.
  * homepage: https://github.com/afeiship/next-filter
  * version: 1.0.0
- * date: 2020-11-22 17:34:00
+ * date: 2020-11-22 17:45:49
  * license: MIT
  */
 

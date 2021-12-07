4977098867df425eb725dc874852d79ba47580b7
afeiship <1290657123@qq.com>
2021-12-07 21:04:03 +0800

    chore: add unit test

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,14 +1,29 @@
 import fn from '../src';
 
-// global.window = { location: { pathname: 'null' } };
-
-// Object.assign(location, { pathname: null });
-
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    console.log(location.href);
-    fn('https://www.baidu.com');
-    console.log(location.href);
-    // expect(fn({ rel: true })).toBe(undefined);
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  test('url open will append anchor to body', () => {
+    fn('https://www.baidu.com', { debug: true });
+    const html = document.body.innerHTML;
+    expect(html).toBe(
+      '<a href="https://www.baidu.com" target="_blank" rel="noopener noreferrer"></a>'
+    );
+  });
+
+  test('url open can change target', () => {
+    fn('https://www.baidu.com', { target: '_self', debug: true });
+    const html = document.body.innerHTML;
+    expect(html).toBe(
+      '<a href="https://www.baidu.com" target="_self" rel="noopener noreferrer"></a>'
+    );
+  });
+
+  test('url open should not append to body when debug not set to true', () => {
+    fn('https://www.baidu.com', { target: '_self' });
+    const html = document.body.innerHTML;
+    expect(html).toBe('');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 const defaults = {
   target: '_blank',
-  rel: 'noopener noreferrer'
+  rel: 'noopener noreferrer',
+  debug: false
 };
 
 export default (url, options?) => {
   const opts = { ...defaults, ...options };
+  const { debug, ...stdOpts } = opts;
   const a = document.createElement('a');
   a.setAttribute('href', url);
-  for (const key in opts) a.setAttribute(key, opts[key]);
+  for (const key in stdOpts) a.setAttribute(key, opts[key]);
+  if (debug) document.body.appendChild(a);
   a.click();
 };

209663b8328ece19b95d223ecc4032e6070fa9b5
afeiship <1290657123@qq.com>
2021-12-07 10:47:28 +0800

    test: tobe continue

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -2,13 +2,13 @@ import fn from '../src';
 
 // global.window = { location: { pathname: 'null' } };
 
-Object.assign(location, { pathname: null });
+// Object.assign(location, { pathname: null });
 
 describe('api.basic', () => {
   test('normail single value case', () => {
     console.log(location.href);
-
-    // fn('https://www.baidu.com');
+    fn('https://www.baidu.com');
+    console.log(location.href);
     // expect(fn({ rel: true })).toBe(undefined);
   });
 });
